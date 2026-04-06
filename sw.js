c44fa59e8bc89a5e10b9266ff9208ee660bf8c24
Jennifer Macbook <jennifermacbook@users-MBP.attlocal.net>
2026-04-06 14:06:34 -0400

    Fix logout, books, Writing quiz, orphaned functions v300

--- sw.js
+++ sw.js
@@ -1,12 +1,7 @@
 const V = 'bk-v300';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(ks => Promise.all(ks.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll({type:'window'}).then(cs => cs.forEach(c => c.navigate(c.url))))
-  );
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request, {cache:'no-store'}).catch(() => caches.match(e.request)));
