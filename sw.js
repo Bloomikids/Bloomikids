0918dd5cce9e167af0a1dd2a453fe8f1bfe56086
Jennifer Macbook <jennifermacbook@users-MBP.attlocal.net>
2026-04-09 22:29:21 -0400

    v324

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BloomiKids SW v320 - force cache bust
-const V = 'bk-v322';
+const V = 'bk-v324';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
