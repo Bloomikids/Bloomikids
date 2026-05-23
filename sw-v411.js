35ad816d133e1574a1b947330d80341edce12d15
Jennifer Macbook <bloomikids21@gmail.com>
2026-05-22 20:37:37 -0400

    v411 unregister old SW, use sw-v411.js

---
+++ sw-v411.js
@@ -0,0 +1,14 @@
+// bk-v411
+const CACHE_NAME = 'bk-v411';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request));
+});
